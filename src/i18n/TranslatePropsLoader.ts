1eec144c9bb3e4d739c7f0b5b439aedaf526505e
Dimuthu Perera <dimuthu.p@creativesoftware.com>
2018-04-03 16:06:34 +0530

    156120274 : Localization /Language support : Review fixes

--- src/i18n/TranslatePropsLoader.ts
+++ src/i18n/TranslatePropsLoader.ts
@@ -24,6 +24,7 @@ export class TranslatePropsLoader implements TranslateLoader {
   public getTranslation(lang: string): Observable<any> {
     return this.http
       .get(`${this.prefix}/${lang}${this.suffix}`, { responseType: "text" })
+      .map()
       .pipe(map((contents: string) => this.parse(contents)));
   }
 
@@ -46,7 +47,6 @@ export class TranslatePropsLoader implements TranslateLoader {
     // console.log(translations);
 
     var data = contents;
-    var mode = "map";
     var parsed = "";
     var parameters = data.split(/\n/);
     var regPlaceHolder = /(\{\d+\})/g;
@@ -76,7 +76,7 @@ export class TranslatePropsLoader implements TranslateLoader {
           value = value.replace(/^\s\s*/, "").replace(/\s\s*$/, ""); // trim
 
           /** Mode: bundle keys in a map */
-          if (mode == "map" || mode == "both") {
+
             // handle unicode chars possibly left out
             var unicodeMatches = value.match(unicodeRE);
             if (unicodeMatches) {
@@ -89,69 +89,12 @@ export class TranslatePropsLoader implements TranslateLoader {
             }
             // add to map
             translations[name] = value;
-          }
-
-          /** Mode: bundle keys as vars/functions */
-          if (mode == "vars" || mode == "both") {
-            // make sure namespaced key exists (eg, 'some.key')
-            this.checkKeyNamespace(name);
-
-            // value with variable substitutions
-            if (regPlaceHolder.test(value)) {
-              var parts = value.split(regPlaceHolder);
-              // process function args
-              var first = true;
-              var fnArgs = "";
-              var usedArgs = [];
-              for (var p = 0; p < parts.length; p++) {
-                if (
-                  regPlaceHolder.test(parts[p]) &&
-                  (usedArgs.length == 0 || usedArgs.indexOf(parts[p]) == -1)
-                ) {
-                  if (!first) {
-                    fnArgs += ",";
-                  }
-                  fnArgs += parts[p].replace(regRepPlaceHolder, "v$1");
-                  usedArgs.push(parts[p]);
-                  first = false;
-                }
-              }
-              parsed += name + "=function(" + fnArgs + "){";
-              // process function body
-              var fnExpr =
-                '"' + value.replace(regRepPlaceHolder, '"+v$1+"') + '"';
-              parsed += "return " + fnExpr + ";" + "};";
-
-              // simple value
-            } else {
-              parsed += name + '="' + value + '";';
-            }
-          } // END: Mode: bundle keys as vars/functions
+          
         } // END: if(pair.length > 0)
       } // END: skip comments
     }
 
-    console.log(translations);
-
     return translations;
-  }
-
-  /** Make sure namespace exists (for keys with dots in name) */
-  checkKeyNamespace(key) {
-    var regDot = /\./;
-    if (regDot.test(key)) {
-      var fullname = "";
-      var names = key.split(/\./);
-      for (var i = 0; i < names.length; i++) {
-        if (i > 0) {
-          fullname += ".";
-        }
-        fullname += names[i];
-        if (typeof fullname == "undefined") {
-          eval(fullname + "={};");
-        }
-      }
-    }
   }
 
   /** Unescape unicode chars ('\u00e3') */
