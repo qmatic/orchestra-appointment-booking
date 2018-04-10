b7e587f040c2a68f0c9ed6ffd76232a1a0888be0
Lakmal Caldera <lakmal.developer@gmail.com>
2018-04-10 13:22:20 +0530

    unsubscribed licenseSubscription

--- src/app/components/containers/qm-app-loader/qm-app-loader.component.ts
+++ src/app/components/containers/qm-app-loader/qm-app-loader.component.ts
@@ -1,19 +1,30 @@
 import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 import { LicenseInfoSelectors } from './../../../../store/services/license.selectors';
-import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'qm-qm-app-loader',
   templateUrl: './qm-app-loader.component.html',
   styleUrls: ['./qm-app-loader.component.scss']
 })
-export class QmAppLoaderComponent {
+export class QmAppLoaderComponent implements OnDestroy {
   // Load other selector in here to make sure they are loaded before app is launched!!!
-  constructor(private licenseSelector: LicenseInfoSelectors, private router: Router) {
-    this.licenseSelector.isLicenseLoaded$.subscribe(loadedState => {
+  licenseSubscription: Subscription;
+  constructor(
+    private licenseSelector: LicenseInfoSelectors,
+    private router: Router
+  ) {
+    this.licenseSubscription = this.licenseSelector.isLicenseLoaded$.subscribe(loadedState => {
       if (loadedState) {
         this.router.navigate(['/app']);
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.licenseSubscription.unsubscribe();
   }
 }
--- src/store/services/license-data.service.ts
+++ src/store/services/license-data.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 import { catchError } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 import { qsystemEndpoint, DataServiceError } from './data.service';
 
