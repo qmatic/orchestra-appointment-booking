f368b980906ba5ca10f66c1d80d11d027a6f62e1
Lakmal Caldera <lakmal.developer@gmail.com>
2018-04-22 13:10:16 +0530

    fixed translation issue in development build in orestra server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  propertiesUrl: '/bundle'
+  production: true
 };
--- src/i18n/TranslationsLoaderFactory.ts
+++ src/i18n/TranslationsLoaderFactory.ts
@@ -2,6 +2,8 @@ import { environment } from './../environments/environment';
 import { TranslatePropsLoader } from './TranslatePropsLoader';
 import { HttpClient } from '@angular/common/http';
 
+const RESOURCE_PATH = '/bundle';
+
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslatePropsLoader(http, environment.propertiesUrl, '.properties');
+  return new TranslatePropsLoader(http, RESOURCE_PATH, '.properties');
 }
