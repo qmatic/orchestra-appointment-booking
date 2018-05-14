d642431ea0a207c8b83060093b9a2bc9eb1ad076
QMATIC\joelan <joel.langstrom@qmatic.com>
2018-05-14 11:41:14 +0200

    156155729 : Select date : Added check if reponse is empty and give toast to indicate that there are no available time slots.

--- src/store/effects/date.effects.ts
+++ src/store/effects/date.effects.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store/src/models';
 import { Effect, Actions } from '@ngrx/effects';
+import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs/Observable';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 import * as AllActions from './../actions';
 import { DateDataService } from '../services';
+import { ToastService } from '../../services/util/toast.service';
 
 const toAction = AllActions.toAction();
 
@@ -13,7 +15,9 @@ const toAction = AllActions.toAction();
 export class DateEffects {
     constructor(
       private actions$: Actions,
-      private dateDataService: DateDataService
+      private dateDataService: DateDataService,
+      private toastService: ToastService,
+      private translateService: TranslateService
     ) {}
 
     @Effect()
@@ -27,6 +31,19 @@ export class DateEffects {
             AllActions.FetchDatesFail
           )
         )
+      );
+
+    @Effect( { dispatch: false })
+    checkIfAvailableDates$: Observable<Action> = this.actions$
+      .ofType(AllActions.FETCH_DATES_SUCCESS)
+      .pipe(
+        tap((action: AllActions.FetchDatesSuccess) => {
+          if (action.payload.dates.length === 0) {
+            this.translateService.get('label.no.dates.available').subscribe(
+              (label: string) => this.toastService.errorToast(label)
+            ).unsubscribe();
+          }
+        })
       );
 
     @Effect()
