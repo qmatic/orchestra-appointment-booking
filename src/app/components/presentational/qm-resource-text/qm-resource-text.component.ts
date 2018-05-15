8f510ebdbdf1a6013bd034fd7e51ab686ad24aa3
QMATIC\joelan <joel.langstrom@qmatic.com>
2018-05-15 08:55:04 +0200

    156500208 : Show name of resource : Added check for admin setting.

--- src/app/components/presentational/qm-resource-text/qm-resource-text.component.ts
+++ src/app/components/presentational/qm-resource-text/qm-resource-text.component.ts
@@ -1,26 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { UserSelectors } from '../../../../store';
+import { UserSelectors, SettingsAdminSelectors } from '../../../../store';
+import { Setting } from '../../../../models/Setting';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'qm-resource-text',
   templateUrl: './qm-resource-text.component.html',
   styleUrls: ['./qm-resource-text.component.scss']
 })
-export class QmResourceTextComponent implements OnInit {
-
+export class QmResourceTextComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription = new Subscription();
   private userDirection$: Observable<string>;
+  private adminSettings$: Observable<{ [name: string]: Setting }>;
+
+  private resourceEnabled: boolean;
 
   @Input()
   text = '';
 
   constructor(
-    private userSelectors: UserSelectors
+    private userSelectors: UserSelectors,
+    private settingsAdminSelectors: SettingsAdminSelectors
   ) {
     this.userDirection$ = this.userSelectors.userDirection$;
+    this.adminSettings$ = this.settingsAdminSelectors.settingsAsMap$;
   }
 
   ngOnInit() {
+    const adminSettingsSubscription = this.adminSettings$.subscribe(
+      (adminSettings: { [name: string]: Setting }) =>
+        this.resourceEnabled = adminSettings.ShowResource.value
+    );
+
+    this.subscriptions.add(adminSettingsSubscription);
   }
 
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  showResource() {
+    return this.text !== '' && this.resourceEnabled;
+  }
 }
