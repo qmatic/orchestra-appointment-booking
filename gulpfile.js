42d125d3987d02d63c748be11d234481876d35e5
Dimuthu Perera <dimuthu.p@creativesoftware.com>
2018-06-05 16:23:32 +0530

    158006442: Manifest file in artifacts

--- gulpfile.js
+++ gulpfile.js
@@ -22,25 +22,25 @@ try {
   var config = require('./gulp.config.json');
 
   // Must be provided via config.gulp.json file
-  var remoteDeployHost = config.remote_deploy.host
-    ? config.remote_deploy.host
-    : remoteDeploymentDefaultHost;
+  var remoteDeployHost = config.remote_deploy.host ?
+    config.remote_deploy.host :
+    remoteDeploymentDefaultHost;
   var remoteDeployUsername = config.remote_deploy.username;
   var remoteDeployPassword = config.remote_deploy.password;
 
   // Artifactory Deployment (artifactory)
-  var targetArtifactoryIp = config.artifactory.host
-    ? config.artifactory.host
-    : '';
-  var targetArtifactoryPath = config.artifactory.path
-    ? config.artifactory.path
-    : '';
-  var targetArtifactoryPort = config.artifactory.port
-    ? config.artifactory.port
-    : '80';
-  var targetArtifactoryProtocol = config.artifactory.protocol
-    ? config.artifactory.protocol
-    : 'http';
+  var targetArtifactoryIp = config.artifactory.host ?
+    config.artifactory.host :
+    '';
+  var targetArtifactoryPath = config.artifactory.path ?
+    config.artifactory.path :
+    '';
+  var targetArtifactoryPort = config.artifactory.port ?
+    config.artifactory.port :
+    '80';
+  var targetArtifactoryProtocol = config.artifactory.protocol ?
+    config.artifactory.protocol :
+    'http';
   var targetArtifactoryUsername = config.artifactory.username;
   var targetArtifactoryPassword = config.artifactory.password;
   var targetArtifactoryUrl =
@@ -56,11 +56,11 @@ try {
 }
 
 // Clean up tasks
-gulp.task('clean:artifactory', function() {
+gulp.task('clean:artifactory', function () {
   return del(['./dist/*', '!./dist/*.zip']);
 });
 
-gulp.task('clean:war', function() {
+gulp.task('clean:war', function () {
   return del([
     './dist/*',
     '!./dist/properties',
@@ -70,17 +70,17 @@ gulp.task('clean:war', function() {
 });
 
 // Copy properties files
-gulp.task('create:properties', function() {
+gulp.task('create:properties', function () {
   return gulp.src(['./src/assets/i18n/*']).pipe(gulp.dest('./dist/properties'));
 });
 
 // Copy release notes
-gulp.task('create:release-notes', function() {
+gulp.task('create:release-notes', function () {
   return gulp.src(['release-notes/**']).pipe(gulp.dest('dist/release-notes/'));
 });
 
 // Create war
-gulp.task('create:war', function() {
+gulp.task('create:war', function () {
   return gulp
     .src(['./dist/**/*'])
     .pipe(zip('appointmentbooking-ui.war'))
@@ -88,9 +88,9 @@ gulp.task('create:war', function() {
 });
 
 // Create artifcatory zip
-gulp.task('create:artifactory:zip', function() {
+gulp.task('create:artifactory:zip', function () {
   try {
-    var appData = JSON.parse(fs.readFileSync('./src/app.json'));
+    var appData = getVersionInfo();
     if (appData) {
       var version = appData.version;
       return gulp
@@ -107,14 +107,13 @@ gulp.task('create:artifactory:zip', function() {
 });
 
 // Write to manifest file
-gulp.task('write:manifest', function() {
+gulp.task('write:manifest', function () {
   try {
-    var appData = JSON.parse(fs.readFileSync('./src/app.json'));
-    if (appData) {
-      var version = appData.version;
-      var fileContent = 'Build-Date: ' + new Date().toISOString().substring(0,10) + '\r\n';      
+    var versionInfo = getVersionInfo();
+    if (versionInfo) {
+      var fileContent = 'Build-Date: ' + new Date().toISOString().substring(0, 10) + '\r\n';
       fileContent += 'Product-Name: Appointment Booking' + '\r\n';
-      fileContent += 'Build-Version: ' + version + '\r\n';
+      fileContent += 'Build-Version: ' + versionInfo.version + '\r\n';
       fs.writeFileSync('./src/META-INF/MANIFEST.MF', fileContent);
       return true;
     }
@@ -126,8 +125,20 @@ gulp.task('write:manifest', function() {
   }
 });
 
+function getVersionInfo() {
+  var appData = JSON.parse(fs.readFileSync('./src/app.json'));
+  if (appData) {
+    return {
+      versionPrefix: appData.version,
+      version: appData.version + '.' + appData.build,
+      build: appData.build
+    };
+  }
+  return null;
+}
+
 // Deploy build to orchestra
-gulp.task('deploy:war', function() {
+gulp.task('deploy:war', function () {
   return gulp.src('./dist/webapp/appointmentbooking-ui.war').pipe(
     sftp({
       remotePath: remoteDeploymentDefaultPath,
@@ -141,13 +152,13 @@ gulp.task('deploy:war', function() {
 });
 
 // Deploy build to artifactory
-gulp.task('deploy:war:artifactory', function() {
+gulp.task('deploy:war:artifactory', function () {
   var warName = fs.readdirSync('./dist')[0];
   var fileExtension = warName.substring(warName.lastIndexOf('.') + 1);
   if (fileExtension === 'zip') {
     ncmd.get(
       `curl -u '${targetArtifactoryUsername}:${targetArtifactoryPassword}' -X PUT ${targetArtifactoryUrl}${targetArtifactoryPath}/${warName} -T ./dist/${warName}`,
-      function(err, data, stderr) {
+      function (err, data, stderr) {
         if (!err) {
           console.log(data);
         } else {
@@ -161,7 +172,7 @@ gulp.task('deploy:war:artifactory', function() {
 });
 
 // Deploy lang file to orchestra
-gulp.task('deploy:lang', function() {
+gulp.task('deploy:lang', function () {
   return gulp
     .src('./dist/properties/appointmentBookingMessages.properties')
     .pipe(
@@ -177,24 +188,24 @@ gulp.task('deploy:lang', function() {
 });
 
 /**
-* Create Dev/Prod build war
-*/
+ * Create Dev/Prod build war
+ */
 gulp.task(
   'build:war:properties',
   gulpsync.sync(['create:war', 'create:properties', 'clean:war'])
 );
 
 /**
-* Build war and deploy war/lang
-*/
+ * Build war and deploy war/lang
+ */
 gulp.task(
   'build:war:deploy',
   gulpsync.sync(['build:war:properties', 'deploy:war', 'deploy:lang'])
 );
 
 /**
-* Create Artifactory build
-*/
+ * Create Artifactory build
+ */
 gulp.task(
   'build:artifactory',
   gulpsync.sync([
@@ -202,15 +213,15 @@ gulp.task(
     'create:properties',
     'create:release-notes',
     'clean:war',
+    'write:manifest',
     'create:artifactory:zip',
-    'clean:artifactory',
-    'write:manifest'
+    'clean:artifactory'
   ])
 );
 
 /**
-* Create Artifactory build and deploy
-*/
+ * Create Artifactory build and deploy
+ */
 gulp.task(
   'build:artifactory:deploy',
   gulpsync.sync(['build:artifactory', 'deploy:war:artifactory'])
