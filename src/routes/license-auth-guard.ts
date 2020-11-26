12edbcfa3d4d6eaf77d233f269e8097738767e68
udara <udara.SampathBandara@qmatic.com>
2020-11-26 13:01:58 +0530

    AB-129: artifact build fail issue fixed

--- src/routes/license-auth-guard.ts
+++ src/routes/license-auth-guard.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LicenseInfoSelectors } from './../store/services/license/license.selectors';
-
+import 'rxjs/Rx';
+import { map } from 'rxjs/operators';
 @Injectable()
 export class LicenseAuthGuard implements CanActivate {
   constructor(private licenseStatusSelector: LicenseInfoSelectors, private router: Router) {}
   canActivate(): Observable<boolean> {
-    return this.licenseStatusSelector.getLicenseInfo$.map(licenseState => {
+    return this.licenseStatusSelector.getLicenseInfo$.pipe(map(licenseState => {
       if (licenseState.loaded) {
          if (licenseState.status) {
             return true;
@@ -19,6 +20,6 @@ export class LicenseAuthGuard implements CanActivate {
         this.router.navigate(['/loading'], { skipLocationChange: true });
         return false;
       }
-    });
+    }));
   }
 }
