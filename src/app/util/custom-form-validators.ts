3c35093f2a07e744c8db0d953efb2b131d4e0273
udara <udara.SampathBandara@qmatic.com>
2020-12-08 09:53:22 +0530

    0000000: artifactory build issue fixed

--- src/app/util/custom-form-validators.ts
+++ src/app/util/custom-form-validators.ts
@@ -1,10 +1,10 @@
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { of ,  Observable } from 'rxjs';
 import { NG_VALIDATORS, FormControl, Validator } from '@angular/forms';
 
 export function whiteSpaceValidator(control: FormControl) {
     const whiteSpaceErrorObject = { 'allwhitespace': true };
-    return of(control.value).
+    return of(control.value).pipe(
         map(v => {
             const isAllWhiteSpace = control.value && control.value.trim().length === 0;
             const isValid: boolean = !isAllWhiteSpace;
@@ -12,7 +12,7 @@ export function whiteSpaceValidator(control: FormControl) {
                 control.setErrors(whiteSpaceErrorObject);
             }
             return !control.dirty || isValid ? null : whiteSpaceErrorObject;
-        });
+        }));
 }
 
 export function validateNotEqualToFactory(text: string) {
